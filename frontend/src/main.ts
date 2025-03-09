e5c7b296fe1b416fde7d25f9136195858327bd6d
Ahmed-ata112 <ahmedatta2108@gmail.com>
2025-03-09 14:09:33 +0100

    add imports

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
-import { CoreModule } from './app/core/core.module';
-import { SharedModule } from './app/shared/shared.module';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
